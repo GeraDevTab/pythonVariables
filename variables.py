bf390e0dd5f5b0684558503141e40b3959e4376c
Gerardo <everythingissofast@gmail.com>
2024-08-30 20:25:05 -0600

    operaciones y formateo de strings

--- variables.py
+++ variables.py
@@ -1,11 +1,22 @@
+#impresion del tipico hola mundo
 print("hello world")
 
+#asignacion de valores a variables
 x=19
 name = "Alice"
 
 print(x)
 print(name)
+
+#declaracion de una variable que va a ser tipo boolean
 is_student = True
 print(is_student)
+
+# reasignacion de valor a una variable
 x= x+1
 print(x)
+
+#uso de las string literales
+name = "Alejandro"
+
+print(f"Hola {name}")
